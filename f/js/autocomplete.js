eeb34edc88b02fe6ae70982d95767de19a41cb9c
Anuradha Naik <anuradha95.naik@gmail.com>
2018-09-07 21:47:53 -0400

    m

--- f/js/autocomplete.js
+++ f/js/autocomplete.js
@@ -1,10 +1,11 @@
-function autocomplete(arr,loader,list,val) {
+function autocomplete(arr,loader,list,valinp) {
 	console.log('autocomplete');
-	$(loader).css('display','none');
+    var val=$(valinp).val();
 	 for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-         console.log(arr[i]);
+         console.log(arr[i]);	$(loader).css('display','none');
+         $(list).css('display','block');
         }
     }
 }
