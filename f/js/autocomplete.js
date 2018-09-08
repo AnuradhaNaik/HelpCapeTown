f2a22fe709c40e56b05969e43e3baf75bad90081
Anuradha Naik <anuradha95.naik@gmail.com>
2018-09-08 02:16:12 -0400

    adding to list

--- f/js/autocomplete.js
+++ f/js/autocomplete.js
@@ -1,11 +1,17 @@
 function autocomplete(arr,loader,list,valinp) {
 	console.log('autocomplete');
     var val=$(valinp).val();
+    var ul=$(list).find("#reslist");
+    $(ul).html('');
+    
+    console.log(ul);
 	 for (i = 0; i < arr.length; i++) {
         /*check if the item starts with the same letters as the text field value:*/
         if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
          console.log(arr[i]);	$(loader).css('display','none');
          $(list).css('display','block');
+         $(ul).append('<li style="">'+arr[i]+'</li>');
         }
+
     }
 }
