c5cfba21b68334b5c801ce6393f7746dee686297
Anuradha Naik <anuradha95.naik@gmail.com>
2018-10-25 22:58:18 -0400

    frontend.js

---
+++ f/js/frontend.js
@@ -0,0 +1,99 @@
+ function fetchhighlights(argument) {
+              // body...
+            }
+            function resultforautocomple(argument){
+              var arr=[];
+              return arr;
+            }
+            
+            function savepost(post){
+              return 0;
+            }
+               function verifypost(post){
+              return 0;
+            }
+            function forwardpost(post){
+              return 0;
+            }
+            function fetchfeed(){
+              return 0;
+            }
+            function fetchApost(){
+              return 0;
+            }
+            function post(argument) {
+              // body...
+              return 0;
+            }
+function getperson(argument) {
+            var result='{}';
+               // body...
+               return result;
+           }
+           function getfollowers(argument) {
+            var result='{}';
+               // body...
+               return result;
+           }
+           function getfollowing(argument) {
+            var result='{}';
+               // body...
+               return result;
+           }
+           
+
+           function editprofile(argument) {
+               // body...
+               return 0;
+           }
+           function fetchsavedfeed(){
+              return 0;
+            }
+              function getmilestone(argument) {
+                   var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getaddto(argument) {
+         var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getquestion(argument) {
+         var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getonemore(argument) {
+         var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getmorefeed(argument) {
+       var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getmilestonefeed(argument) {
+       var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getaddtofeed(argument) {
+       var result='{}';
+               // body...
+               return result;
+        // body...
+    }
+    function getquestionfeed(argument) {
+      var result='{}';
+               // body...
+               return result;
+        // body...
+    }
